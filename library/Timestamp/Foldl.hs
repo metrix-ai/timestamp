b419277672ff9d5fd23b4b19ee914f054402567e
Mr-Patty <opavlyuchenkov@gmail.com>
2018-04-12 12:28:08 +0300

    Add foldl

---
+++ library/Timestamp/Foldl.hs
@@ -0,0 +1,30 @@
+{-|
+Folds on timestamped values
+-}
+module Timestamp.Foldl
+where
+
+import Timestamp.Prelude hiding (maximum)
+import Timestamp
+import Control.Foldl
+
+
+untimestamped :: Fold input output -> Fold (Timestamped input) output
+untimestamped =
+  premap (\ (Timestamped _ x) -> x)
+
+filteringByUtcTime :: (UTCTime -> Bool) -> Fold (Timestamped input) output -> Fold (Timestamped input) output
+filteringByUtcTime filter =
+  filteringByTimestamp (filter . timestampUtcTime)
+
+filteringByTimestamp :: (Timestamp -> Bool) -> Fold (Timestamped input) output -> Fold (Timestamped input) output
+filteringByTimestamp filter =
+  prefilter (\ (Timestamped ts _) -> filter ts)
+
+inUtcTimeRange :: UTCTime -> UTCTime -> Fold (Timestamped input) output -> Fold (Timestamped input) output
+inUtcTimeRange start end =
+  inTimestampRange (utcTimeTimestamp start) (utcTimeTimestamp end)
+
+inTimestampRange :: Timestamp -> Timestamp -> Fold (Timestamped input) output -> Fold (Timestamped input) output
+inTimestampRange start end =
+  filteringByTimestamp (\ ts -> ts >= start && ts < end)
